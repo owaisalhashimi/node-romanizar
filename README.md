71785115f4a9768eb517ebf6fef42745de9f7939
Owais Al-Hashmi <owaisalhashimi@gmail.com>
2013-11-20 19:44:02 +0300

    Prettier code, complete README

--- README.md
+++ README.md
@@ -9,46 +9,34 @@ This tool replaces simple "markup" in HTML (or any text) files with HTML entitie
 
 ### In Text ###
 
-'/AA/'
-'/aa/'
-'/EE/'	
-'/ee/'
-'/OO/'
-'/oo/'
-'/DD/'
-'/dd/'
-'/HH/'
-'/hh/'
-'/SS/'
-'/ss/'
-'/TT/'
-'/tt/'
-'/ZZ/'
-'/zz/'
-'/cc/'
-'/,,/'	
+The markup on the left encodes the characters on the right:
 
-'&#256;'
-'&#257;'
-'&#298;'
-'&#299;'
-'&#362;'
-'&#363;'
-'&#7692;'
-'&#7693;'
-'&#7716;'
-'&#7717;'
-'&#7778;'
-'&#7779;'
-'&#7788;'
-'&#7789;'
-'&#7826;'
-'&#7827;'
-'&#703;'
-'&#702;'
+- '/AA/' &rarr; '&#256;'
+- '/aa/' &rarr; '&#257;'
+- '/EE/' &rarr; '&#298;'
+- '/ee/' &rarr; '&#299;'
+- '/OO/' &rarr; '&#362;'
+- '/oo/' &rarr; '&#363;'
+- '/DD/' &rarr; '&#7692;'
+- '/dd/' &rarr; '&#7693;'
+- '/HH/' &rarr; '&#7716;'
+- '/hh/' &rarr; '&#7717;'
+- '/SS/' &rarr; '&#7778;'
+- '/ss/' &rarr; '&#7779;'
+- '/TT/' &rarr; '&#7788;'
+- '/tt/' &rarr; '&#7789;'
+- '/ZZ/' &rarr; '&#7826;'
+- '/zz/' &rarr; '&#7827;'
+- '/cc/' &rarr; '&#703;'
+- '/,,/' &rarr;	'&#702;'
 
 ### Command Line ###
 
-<pre><code>romanizar -i &lt;path to input file&gt; [-o &lt;path to output file&gt;]</code></pre>
+<pre><code>romanizar -i /path/to/input/file.html [-o /path/to/output/file.html]</code></pre>
 
-If the output is not specified, the produced file will be saved in the same folder as the input file, and its name will be: <pre>&lt;input-filename&gt;-romanized-ar.&lt;extension&gt;</pre>
+If the output is not specified, the produced file will be saved in the same folder as the input file, and will be named as follows:
+
+Given:
+<pre>[input-filename].html</pre>
+Filename:
+<pre>[input-filename]-romanized-ar.html</pre>
--- romanizar.js
+++ romanizar.js
@@ -2,34 +2,76 @@ var fs = require('fs');
 var path = require('path');
 var opts = require('optimist');
 var oTR = {};
-	oTR.args = opts.argv;
-	oTR.file = path.resolve(oTR.args["i"]);
-	oTR.dirname = path.dirname(oTR.file);
-	oTR.extname = path.extname(oTR.file);
-	oTR.filename = path.basename(oTR.file,oTR.extname);
-	oTR.outfilename = (oTR.args['o']) ? path.resolve(oTR.dirname,oTR.args['o']) : oTR.file.replace(oTR.filename,oTR.filename+'-romanized-ar');
-	oTR.transliterate = function(oErr,sFc) {
-		if (oErr) {
+oTR.args = opts.argv;
+oTR.file = path.resolve(oTR.args["i"]);
+oTR.dirname = path.dirname(oTR.file);
+oTR.extname = path.extname(oTR.file);
+oTR.filename = path.basename(oTR.file,oTR.extname);
+oTR.outfilename = (oTR.args['o']) 
+	? path.resolve(oTR.dirname,oTR.args['o'])
+	: oTR.file.replace(oTR.filename,oTR.filename+'-romanized-ar');
+oTR.transliterate = function(oErr,sFc) {
+	if (oErr) {
+		throw(oErr);
+	};
+	var aPatterns = [
+	'/AA/',
+	'/aa/',
+	'/EE/',
+	'/ee/',
+	'/OO/',
+	'/oo/',
+	'/DD/',
+	'/dd/',
+	'/HH/',
+	'/hh/',
+	'/SS/',
+	'/ss/',
+	'/TT/',
+	'/tt/',
+	'/ZZ/',
+	'/zz/',
+	'/cc/',
+	'/,,/'
+	];
+	
+	var aReplacements = [
+	'&#256;',
+	'&#257;',
+	'&#298;',
+	'&#299;',
+	'&#362;',
+	'&#363;',
+	'&#7692;',
+	'&#7693;',
+	'&#7716;',
+	'&#7717;',
+	'&#7778;',
+	'&#7779;',
+	'&#7788;',
+	'&#7789;',
+	'&#7826;',
+	'&#7827;',
+	'&#703;',
+	'&#702;'
+	];
+
+	aPatterns.forEach(function(sPattern,i){
+		var sRegExp = new RegExp(sPattern,'g');
+		 sFc = sFc.replace(sRegExp,aReplacements[i]);
+	});
+
+	fs.writeFile(oTR.outfilename,sFc,function(oErr){
+		if (oErr){
 			throw(oErr);
 		};
-		var aPatterns = ['/AA/', '/aa/', '/EE/', '/ee/', '/OO/', '/oo/', '/DD/', '/dd/', '/HH/', '/hh/', '/SS/', '/ss/', '/TT/', '/tt/', '/ZZ/', '/zz/', '/cc/', '/,,/'];
-		
-		var aReplacements = ['&#256;', '&#257;', '&#298;', '&#299;', '&#362;', '&#363;', '&#7692;', '&#7693;', '&#7716;', '&#7717;', '&#7778;', '&#7779;', '&#7788;', '&#7789;', '&#7826;', '&#7827;', '&#703;', '&#702;'];
+		console.log('Romanized file saved: '+oTR.outfilename);
+	});
 
-		aPatterns.forEach(function(sPattern,i){
-			var sRegExp = new RegExp(sPattern,'g');
-			 sFc = sFc.replace(sRegExp,aReplacements[i]);
-		});
+};
 
-		fs.writeFile(oTR.outfilename,sFc,function(oErr){
-			if (oErr){
-				throw(oErr);
-			};
-			console.log('Romanized file saved: '+oTR.outfilename);
-		});
+oTR.read = function(){
+	fs.readFile(oTR.file,'utf8',oTR.transliterate);
+};
 
-	};
-	oTR.read = function(){
-		fs.readFile(oTR.file,'utf8',oTR.transliterate);
-	};
 module.exports = oTR;
